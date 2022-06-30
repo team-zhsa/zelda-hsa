ff2fd345613f8dbb9c8f3dc479029c74f2a86052
team-zhsa <83789316+team-zhsa@users.noreply.github.com>
2022-07-01 00:02:51 +0200

    Alpha 1.14: Mountains
    Teletransporters + G1

--- maps/inside/caves/mountains/to_west_upper_mountains_b1.lua
+++ maps/inside/caves/mountains/to_west_upper_mountains_b1.lua
@@ -1,19 +1,14 @@
--- Lua script of map inside/caves/mountains/to_west_upper_mountain_b1.
--- This script is executed every time the hero enters this map.
-
--- Feel free to modify the code below.
--- You can add more events and remove the ones you don't need.
-
--- See the Solarus Lua API documentation:
--- http://www.solarus-games.org/doc/latest
-
 local map = ...
 local game = map:get_game()
+local audio_manager = require("scripts/audio_manager")
+local door_manager = require("scripts/maps/door_manager")
+local enemy_manager = require("scripts/maps/enemy_manager")
+local separator_manager = require("scripts/maps/separator_manager")
+local switch_manager = require("scripts/maps/switch_manager")
+local treasure_manager = require("scripts/maps/treasure_manager")
 
 -- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
+map:register_event("on_started", function()
+	separator_manager:manage_map(map)
 
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
-end
-
+end)
