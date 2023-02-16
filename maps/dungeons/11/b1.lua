8c2dd0276acd55dc28f98610a86bd96db47e9871
team-zhsa <83789316+team-zhsa@users.noreply.github.com>
2023-02-16 21:54:47 +0100

    Alpha 1.15.3: Dungeon 11

--- maps/dungeons/11/b1.lua
+++ maps/dungeons/11/b1.lua
@@ -9,13 +9,23 @@
 
 local map = ...
 local game = map:get_game()
+local audio_manager = require("scripts/audio_manager")
+local door_manager = require("scripts/maps/door_manager")
+local enemy_manager = require("scripts/maps/enemy_manager")
+local separator_manager = require("scripts/maps/separator_manager")
+local switch_manager = require("scripts/maps/switch_manager")
+local treasure_manager = require("scripts/maps/treasure_manager")
 
 -- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
-
+map:register_event("on_started", function()
+  separator_manager:manage_map(map)
+  map:set_doors_open("door_9_w", false)
+  door_manager:open_when_torches_lit(map, "torch_2_door_", "door_8_e")
+  door_manager:close_when_torches_unlit(map, "torch_2_door_", "door_8_e")
+  door_manager:open_when_switch_activated(map, "switch_9_door", "door_9_w")
   -- You can initialize the movement and sprites of various
   -- map entities here.
-end
+end)
 
 -- Event called after the opening transition effect of the map,
 -- that is, when the player takes control of the hero.
