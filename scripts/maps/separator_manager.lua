7334f444879552dfa946a44947695ef6d2dde83e
team-zhsa <83789316+team-zhsa@users.noreply.github.com>
2023-02-19 01:03:19 +0100

    Alpha 1.15.3: Dungeon 5 collapsing floor

--- scripts/maps/separator_manager.lua
+++ scripts/maps/separator_manager.lua
@@ -69,10 +69,10 @@ function separator_manager:manage_map(map)
     local hero = map:get_hero()
     hero.respawn_point_saved=nil
     local directions={
-      {16,0},
-      {0,-16},
-      {-16,0},
-      {0,16}
+      {32,0},
+      {0,-32},
+      {-32,0},
+      {0,32}
     }
     local x,y,layer=hero:get_position()
     local offset_x, offset_y=unpack(directions[hero:get_direction()+1])
