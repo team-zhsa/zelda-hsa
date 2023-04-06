3172e646f5d7ce12989c65220492bda4005bdf5d
team-zhsa <83789316+team-zhsa@users.noreply.github.com>
2023-04-06 14:35:17 +0200

    Alpha 1.16.1: Houses

--- maps/inside/houses/north_west_castle/poultry_shop.lua
+++ maps/inside/houses/north_west_castle/poultry_shop.lua
@@ -1,4 +1,4 @@
--- Lua script of map inside/houses/north_west_castle/poultry_shop.
+-- Lua script of map inside/houses/kakarico/south_shop.
 -- This script is executed every time the hero enters this map.
 
 -- Feel free to modify the code below.
@@ -10,15 +10,11 @@
 local map = ...
 local game = map:get_game()
 
--- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
-
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
+function sensor_shop_welcome:on_activated()
+  game:start_dialog("maps.houses.north_west_castle.shop.merchant_welcome")
+  sensor_shop_welcome:remove()
 end
 
--- Event called after the opening transition effect of the map,
--- that is, when the player takes control of the hero.
-function map:on_opening_transition_finished()
-
+function merchant:on_interaction()
+  game:start_dialog("maps.houses.north_west_castle.shop.merchant_welcome")
 end
