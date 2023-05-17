90942b35d79812125cb3342bac2930285a7c43cd
team-zhsa <83789316+team-zhsa@users.noreply.github.com>
2023-05-17 12:53:21 +0200

    Alpha 1.17: Sea maps
    Need to update the teletransporters and the coordinates

---
+++ maps/out/f9.lua
@@ -0,0 +1,24 @@
+-- Lua script of map out/l8.
+-- This script is executed every time the hero enters this map.
+
+-- Feel free to modify the code below.
+-- You can add more events and remove the ones you don't need.
+
+-- See the Solarus Lua API documentation:
+-- http://www.solarus-games.org/doc/latest
+
+local map = ...
+local game = map:get_game()
+
+-- Event called at initialization time, as soon as this map is loaded.
+map:register_event("on_started", function()
+  -- You can initialise the movement and sprites of various
+  
+	map:set_digging_allowed(true)
+end)
+
+-- Event called after the opening transition effect of the map,
+-- that is, when the player takes control of the hero.
+function map:on_opening_transition_finished()
+
+end
