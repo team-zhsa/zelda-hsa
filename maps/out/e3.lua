711b6a38e40c0468e7167a515b0673ea6e51e4b5
team-zhsa <83789316+team-zhsa@users.noreply.github.com>
2023-07-08 16:13:28 +0200

    Alpha 1.17: Dungeon 6

--- maps/out/e3.lua
+++ maps/out/e3.lua
@@ -10,15 +10,7 @@
 local map = ...
 local game = map:get_game()
 
--- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
-
-  -- You can initialise the movement and sprites of various
-  -- map entities here.
-end
-
--- Event called after the opening transition effect of the map,
--- that is, when the player takes control of the hero.
-function map:on_opening_transition_finished()
-
-end
+map:register_event("on_started", function()
+	map:set_digging_allowed(true)
+  game:show_map_name("north_field")
+end)
