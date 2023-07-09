46d664267b8100b70dd034bbe7e38a82ab2929d6
team-zhsa <83789316+team-zhsa@users.noreply.github.com>
2023-07-09 18:23:12 +0200

    Alpha 1.17: Green Soldier weapon

--- enemies/soldiers/sword_soldier_green.lua
+++ enemies/soldiers/sword_soldier_green.lua
@@ -1,28 +1,114 @@
+-- Global variables
 local enemy = ...
-local audio_manager = require("scripts/audio_manager.lua")
-local going_hero = false
-local being_pushed = false
-local main_sprite = nil
-local sword_sprite = nil
+require("enemies/lib/common_actions").learn(enemy)
+require("enemies/lib/weapons").learn(enemy)
+
+local game = enemy:get_game()
+local map = enemy:get_map()
+local hero = map:get_hero()
+local sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
+local quarter = math.pi * 0.5
+local is_charging = false
+local is_waking_up = false
 
 -- Configuration variables
-local detect_distance = 96
-local normal_speed = 32
-local fast_speed = 48
+local charge_triggering_distance = 64
+local charging_speed = 56
+local walking_angles = {0, quarter, 2.0 * quarter, 3.0 * quarter}
+local walking_speed = 32
+local walking_minimum_distance = 16
+local walking_maximum_distance = 96
+local waiting_duration = 800
+local waiting__ = 50
 
-function enemy:on_created()
-	enemy:set_life(2)
-	enemy:set_damage(2)
-	enemy:set_hurt_style("normal")
-	sword_sprite = enemy:create_sprite("enemies/" .. enemy:get_breed() .."_weapon")
-	main_sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
-	enemy:set_size(16, 16)
-	enemy:set_origin(8, 13)
+-- Start the enemy charge movement.
+function start_charging()
 
-	enemy:set_invincible_sprite(sword_sprite)
-	enemy:set_attack_consequence_sprite(sword_sprite, "sword", "custom")
+  is_charging = true
+  enemy:stop_movement()
+  enemy:start_target_walking(hero, charging_speed)
+  sprite:set_animation("walking")
 end
 
+function look_around()
+	local looking_animation = "looking_around"
+	local next_direction = math.random(4)
+	sprite:set_animation(looking_animation)
+	sol.timer.start(enemy, 1600, function()
+		start_walking(next_direction)
+	end)
+end
+
+-- Start the enemy random movement.
+function start_walking(direction)
+	is_charging = false
+  direction = direction or math.random(4)
+  enemy:start_straight_walking(walking_angles[direction], walking_speed, math.random(walking_minimum_distance, walking_maximum_distance), function()
+		
+		local waiting_animation = "immobilized"
+		sprite:set_animation(waiting_animation)
+		
+    sol.timer.start(enemy, waiting_duration, function()
+      if not is_charging then
+				look_around()
+      end
+    end)
+  end)
+end
+
+-- Passive behaviours needing constant checking.
+enemy:register_event("on_update", function(enemy)
+
+  if enemy:is_immobilized() then
+    return
+  end
+
+  -- Start charging if the hero is near enough
+  if not is_charging and enemy:is_near(hero, charge_triggering_distance) then
+    start_charging()
+	elseif is_charging and not enemy:is_near(hero, charge_triggering_distance) then
+		start_walking()
+  end
+end)
+
+-- Initialization.
+enemy:register_event("on_created", function(enemy)
+
+  enemy:set_life(2)
+  enemy:set_size(16, 16)
+  enemy:set_origin(24, 29)
+  enemy:hold_weapon("enemies/"..enemy:get_breed().."_weapon",enemy:get_sprite(), 0, 0)
+end)
+
+-- Restart settings.
+enemy:register_event("on_restarted", function(enemy)
+
+  enemy:set_hero_weapons_reactions({
+  	arrow = 2,
+  	boomerang = 2,
+  	explosion = 2,
+  	sword = 1,
+  	thrown_item = 2,
+  	fire = 2,
+  	jump_on = "ignored",
+  	hammer = 2,
+  	hookshot = 2,
+  	magic_powder = 2,
+  	shield = "protected",
+  	thrust = 2
+  })
+
+  -- States.
+  enemy:set_can_attack(true)
+  enemy:set_damage(2)
+	enemy:set_drawn_in_y_order()
+	if is_charging then
+		start_charging()
+	else
+		start_walking()
+	end
+end)
+--[[
 function enemy:on_restarted()
 	if not being_pushed then
 		if going_hero then
@@ -112,4 +198,4 @@ function enemy:on_collision_enemy(other_enemy, other_sprite, my_sprite)
 		enemy:set_traversable(true) --default false
 		sol.timer.start(200, function() enemy:set_traversable(true) end)
 	end
-end
+end--]]
