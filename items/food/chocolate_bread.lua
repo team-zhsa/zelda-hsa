5418f4415bac58039fbca1e4700b89fa22995b4d
team-zhsa <83789316+team-zhsa@users.noreply.github.com>
2023-12-21 21:21:30 +0100

    Alpha 1.17: After exam commit

--- items/food/chocolate_bread.lua
+++ items/food/chocolate_bread.lua
@@ -1,11 +1,13 @@
 local item = ...
 
 function item:on_created()
-
-  self:set_savegame_variable("item_status_pains_au_chocolat_counter")
+  self:set_savegame_variable("possession_chocolate_bread")
+  self:set_amount_savegame_variable("amount_chocolate_bread")
+  self:set_brandish_when_picked(false)
+  self:set_sound_when_picked("common/get_small_item1")
+  self:set_sound_when_brandished("common/chest_minor_item")
   self:set_assignable(true)
-  self:set_amount_savegame_variable("item_status_pains_au_chocolat_amount")
-  self:set_max_amount(10)
+  self:set_max_amount(256)
 end
 
 function item:on_using()
@@ -14,8 +16,12 @@ function item:on_using()
     sol.audio.play_sound("wrong")
   else
     self:remove_amount(1)
-    self:get_game():add_life(4 * 3)
+    self:get_game():add_life(6)
   end
   self:set_finished()
 end
 
+function item:on_obtaining(variant, savegame_variable)
+
+  self:add_amount(1)
+end
