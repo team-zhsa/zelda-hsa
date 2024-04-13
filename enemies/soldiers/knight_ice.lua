39ca9a8bbc308befbfad275f8063d4f1c9f2a254
team-zhsa <83789316+team-zhsa@users.noreply.github.com>
2024-04-13 18:09:45 +0200

    Alpha 1.17: Dungeon 16

--- enemies/soldiers/knight_ice.lua
+++ enemies/soldiers/knight_ice.lua
@@ -19,8 +19,11 @@ local map = enemy:get_map()
 local hero = map:get_hero()
 local sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
 local quarter = math.pi * 0.5
+local is_charging = false
 
 -- Configuration variables.
+local charge_triggering_distance = 80
+local charging_speed = 56
 local walking_angles = {0, quarter, 2.0 * quarter, 3.0 * quarter}
 local walking_speed = 48
 local walking_minimum_distance = 16
@@ -31,17 +34,24 @@ local throwing_duration = 200
 local projectile_breed = "projectiles/spear"
 local projectile_offset = {{0, -14}, {6, -4}, {0, -14}, {-1, -4}}
 
+-- Start the enemy charge movement.
+function enemy:start_charging()
+  is_charging = true
+  enemy:stop_movement()
+  enemy:start_target_walking(hero, charging_speed)
+  sprite:set_animation("walking")
+	sol.audio.play_sound("hero/hero_seen")
+end
+
 -- Start the enemy movement.
 function enemy:start_walking(direction)
-
+  is_charging = false
 	direction = direction or math.random(4)
 	enemy:start_straight_walking(walking_angles[direction], walking_speed, math.random(walking_minimum_distance, walking_maximum_distance), function()
 		local next_direction = math.random(4)
 		local waiting_animation = "looking_around"
 		sprite:set_animation(waiting_animation)
-
 		sol.timer.start(enemy, waiting_duration, function()
-
 			-- Throw an arrow if the hero is on the direction the enemy is looking at.
 			if enemy:get_direction4_to(hero) == sprite:get_direction() then
 				enemy:throw_projectile(projectile_breed, throwing_duration, projectile_offset[direction][1], projectile_offset[direction][2], function()
@@ -54,6 +64,24 @@ function enemy:start_walking(direction)
 	end)
 end
 
+-- Passive behaviors needing constant checking.
+function enemy:check_hero()
+  if enemy:is_immobilized() then
+    return
+  end
+  -- Start charging if the hero is near enough
+  if not is_charging and enemy:is_near(hero, charge_triggering_distance) then
+    enemy:start_charging()
+	elseif is_charging and not enemy:is_near(hero, charge_triggering_distance) then
+		enemy:start_walking()
+	elseif is_charging and enemy:is_near(hero, charge_triggering_distance) then
+		enemy:throw_projectile(projectile_breed, throwing_duration, projectile_offset[math.random(4)][1], projectile_offset[math.random(4)][2], function()
+			enemy:start_walking()
+		end)
+  end
+	sol.timer.start(enemy, 1000, function() enemy:check_hero() end)
+end
+
 -- Initialization.
 enemy:register_event("on_created", function(enemy)
 	enemy:hold_weapon("enemies/soldiers/knight_spear", enemy:get_sprite(), 0, 0)
@@ -64,7 +92,6 @@ end)
 
 -- Restart settings.
 enemy:register_event("on_restarted", function(enemy)
-
 	enemy:set_hero_weapons_reactions({
 		arrow = 2,
 		boomerang = 2,
@@ -79,9 +106,13 @@ enemy:register_event("on_restarted", function(enemy)
 		shield = "protected",
 		thrust = 2
 	})
-
 	-- States.
 	enemy:set_can_attack(true)
 	enemy:set_damage(8)
-	enemy:start_walking()
+	if is_charging and enemy:is_near(hero, charge_triggering_distance) then
+		enemy:start_charging()
+	else
+		enemy:start_walking()
+	end
+	enemy:check_hero()
 end)
