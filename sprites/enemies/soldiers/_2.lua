5a0179add416ade8736b098f3cfa344a9adaabc0
team-zhsa <83789316+team-zhsa@users.noreply.github.com>
2024-04-14 02:47:50 +0200

    Alpha 1.17: Like-Like & Cordinia
    #221

--- sprites/enemies/soldiers/_2.lua
+++
@@ -1,9 +0,0 @@
--- Butter (has several variables)
-local item = ...
-
-function item:on_created()
-
-  self:set_savegame_variable("possession_butter")
-  self:set_sound_when_brandished("items/sidequest_major_item")
-
-end
