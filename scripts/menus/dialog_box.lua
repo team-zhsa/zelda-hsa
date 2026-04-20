1535729b2ae272c9fa68b9699a6385f2ca73d265
team-zhsa <83789316+team-zhsa@users.noreply.github.com>
2026-04-21 00:56:22 +0200

    Alpha 1.17: Dialog box removed stopped animation Closes #262

--- scripts/menus/dialog_box.lua
+++ scripts/menus/dialog_box.lua
@@ -177,9 +177,6 @@ local function initialise_dialog_box_features(game)
 		local map = game:get_map()
 		local camera_x, camera_y, camera_width, camera_height = map:get_camera():get_bounding_box()
 		local hero = map:get_entity("hero")
-		if hero:get_animation() == "walking" then
-			hero:set_animation("stopped")
-		end
 		local top = false
 		if self.vertical_position == "top" then
 			top = true
